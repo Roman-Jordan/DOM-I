fe86995962d4365f9e1c0cbbb8f8077b3df39c43
AlphaRomulus <rjordan1352@gmail.com>
2019-04-08 16:25:04 -0700

    Internet was week at my house, up and running now, initial commit.

--- js/index.js
+++ js/index.js
@@ -38,5 +38,33 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+// function byClass(class,operator, obj,prop){
+//   let select = document.getElementsByClassName(class);
+//   const operation ={
+//     0 : select.setAttribute(prop[3], siteContent[prop[1]][prop[2]]),
+//     1 : select.innerHTML = siteContent[prop[1]][prop[2]],
+//   }
+//   return operation.operator;
+// }
+
+
+function byId(id,operator, obj,prop){
+  let select = document.getElementById(id);
+  const operation ={
+    Attr : function(){select.setAttribute(prop[2], obj[prop[0]][prop[1]])},
+    HTML : function(){select.innerHTML = obj[prop[1]][prop[2]]}
+  }
+  return operation[operator]();
+}
+byId('logo-img','Attr',siteContent,['nav','img-src','src']);
+byId('cta-img','Attr',siteContent,['cta','img-src','src']);
+
+
+// Header Nav Loop
+let navItems = siteContent.nav;
+let navSelector = document.querySelectorAll("header nav a");
+navSelector.forEach((x,i) => {
+    x.innerHTML = navItems[String(`nav-item-${i + 1}`)];
+});
+
+
